e2c1f3bbb0e49a7d35c4d66081f6ecf1d05c9b14
Ulysse Renzetti <ulysse.renzetti@outlook.fr>
2024-11-28 17:17:32 +0100

    cool

--- jeux_pendu/correction.py
+++ jeux_pendu/correction.py
@@ -1,18 +1,27 @@
+# import modules
 import random
+#mots
 words=["hello","world","python","tree","conversly","three","congatrulations","apple"]
+# tirage au sort
 wordChosen=words[random.randint(0,7)]
+# variables 
 nombreVie=8
 motActuel=[]
 #print(wordChosen)
+# ajouter autant tirets que de lettres dans le mot
 for i in range(len(wordChosen)):
     motActuel.append('_')
+# boucle principale qui s'arrête lorqu'il ya plus de vies.
 for i in range(nombreVie):
+# input
     lettreChoisie=input("Devinez une lettre: ")
+# Si la lettre de l'utilisateur est dans le mot alors le signaler
     if  lettreChoisie in wordChosen:
         print("lettre trouvée!")
         for j in range(len(wordChosen)):
             if lettreChoisie == wordChosen[j]:
                 motActuel[j] = lettreChoisie
+    # si le mot est complet alors afficher le mot
     if "".join(motActuel) == wordChosen:
         print("Vous avez trouvé:", motActuel)
         break
