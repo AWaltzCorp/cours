0dcc9c8f59d4063d7335b108d995cc439eea1b96
Ulysse Renzetti <ulysse.renzetti@outlook.fr>
2025-01-30 17:50:18 +0100

    cooo

---
+++ devoirs/turtle_exo.py
@@ -0,0 +1,11 @@
+import turtle
+# Initialisation
+t = turtle.Turtle()
+# Déplacement simple
+t.forward(100)  # Avance de 100 unités
+t.right(90)     # Tourne à droite de 90°
+t.forward(50)   # Avance de 50 unités
+t.left(90)
+t.forward(70)
+
+turtle.done()
